5a9ed01afe5c12c3a9912d3479705d6ecb6cb2b4
victorHugoAguilar <victorhugoaguilar@aol.com>
2020-07-24 10:03:29 +0200

    Añadir DragandDrop a los componentes listTrello and CardTrello, tambien a App.js

--- src/components/TrelloList/TrelloList.jsx
+++ src/components/TrelloList/TrelloList.jsx
@@ -1,24 +1,36 @@
 import React from 'react';
+// Importamos los estilos personalizados para el componente
 import './TrelloList.scss';
-
+// Importamos componentes para DropAndDrag
+import { Droppable } from 'react-beautiful-dnd';
 // Importamos los compoenentes personalizados
 import TrelloCard from '../TrelloCard/TrelloCard';
 import TrelloActionButton from '../TrelloActionButton/TrelloActionButton';
 
 const TrelloList = ({ title, cards, listID }) => {
-
     return (
-        <div className="trelloList">
-            <h4>{title}</h4>
-            {
-                cards?.map(card => (
-                    <TrelloCard key={card.id} text={card.text} />
-                ))
-            }
-            <TrelloActionButton listID={listID}/>
-        </div>
+        <Droppable droppableId={ String(listID) }>
+            {provided => (
+                <div {...provided.droppableProps}
+                    ref={provided.innerRef}
+                    className="trelloList">
+                    <h4>{title}</h4>
+                    {
+                        cards?.map((card, index) => (
+                            <TrelloCard
+                                key={card.id}
+                                id={card.id}
+                                text={card.text}
+                                index={index}
+                            />
+                        ))
+                    }
+                    <TrelloActionButton listID={listID} />
+                    {provided.placeholder}
+                </div>
+            )}
+        </Droppable>
     );
-
 }
 
 export default TrelloList;
